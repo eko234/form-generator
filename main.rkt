0b6c75b0d52455cd775759a14a9f0fe2439a71b1
jujo <lu.al.bu@hotmail.com>
2020-11-25 18:23:27 -0500

    code form almost done

--- main.rkt
+++ main.rkt
@@ -1,8 +1,9 @@
 #lang racket
 
 (require racket/contract)
+(require racket/pretty)
 
-(define input-types (list 'STRING 'INT 'CASH 'DATE 'PICK-FIXED 'PICK-SOURCE))
+(define input-types (list 'STR 'INT 'CASH 'DATE 'PICK-F 'PICK-S))
 
 (define file-design-template
 "
@@ -18,7 +19,7 @@ uses
 type
   TFRM<UNITNAME> = class(TUniForm)
     procedure UniFormCreate(Sender: TObject);
-  ~a
+~a
   private
   public
   end;
@@ -35,8 +36,6 @@ end;
 end.
 ")
 
-(define (mk-prop field)
-    (~a #:separator "\n" ( fields)))
 
 (define (prepare-template template unit-name)
     (string-replace template "<UNITNAME>" unit-name))
@@ -45,20 +44,30 @@ end.
     (define template_ (prepare-template file-design-template (<form>-title form)))
     (raise "TODO"))
 
+(struct <field> (label input-type source options constraints))
 
-(define (mk-file-code form)
-    (define template_ (prepare-template file-code-template (<form>-title form)))
-    (match form-data
-        [(list a b) 
-            (format template_ a b)
-            (raise "TODO")]
-        [_ (raise "wrong pattern boy")]))
+(struct <form> ( unit-name
+                 title
+                 fields ))
 
-(print (mk-file-form "RECAUDO" (list)))
+(define (field->design field)
+  (raise "TODO"))
 
-(struct <field> ( label input-type source [options null] [constraints null] ) )
+(define (field->code field)
+  (match field
+     [(<field> label input-type source options constraints)
+      (~a #:separator "\n" 
+          (format "    uniLabel~a: TUniLabel;" source)
+          (match input-type
+           ['STR    (format "    uniDBEdit~a: TUniDBEdit;"                     source)]
+           ['INT    (format "    uniDBNumberEdit~a: TUniDBNumberEdit;"         source)]
+           ['CASH   (format "    uniDBNumberEdit~a: TUniDBNumberEdit;"         source)]
+           ['DATE   (format "    uniDBDateTimePicker~a: TUniDBDateTimePicker;" source)]
+           ['PICK-F (format "    uniDBComboBox~a: TUniDBComboBox;"             source)]
+           ['PICK-S (format "    uniDBComboBox~a: TUniDBComboBox;"             source)]))]))
+
 (define (mk-<field> label input-type source [options null] [constraints null])
-    (<input> input-type source options constraints))
+    (<field> label input-type source options constraints))
 
 ; add contract to make options mandatory if pick fixed is chosed
 ; options can be a select statement that determines the posible values a select can take
@@ -67,14 +76,19 @@ end.
     (<form> "INFORMACION_LIQUIDACION"
             "informacion para efectuar liquidacion" 
             (list (mk-<field> "Vigencia"            'DATE        'LIQ-VIG  )
-                  (mk-<field> "Actividad Economica" 'PICK-SOURCE 'ACT-ECO "select * from actividades-economicas" )
+                  (mk-<field> "Actividad Economica" 'PICK-S      'ACT-ECO "select * from actividades-economicas" )
                   (mk-<field> "Ingresos Brutos"     'CASH        'LIQ-ING )
                   (mk-<field> "Ingresos Fuera Mun"  'CASH        'LIQ-ING-EXT )
                   (mk-<field> "Ingresos Netos"      'CASH        'LIQ-ING ))))
 
 ; the most important thing is the datasource of the liquidation, there the checks must be very precise
+                
+(define (mk-file-code form)
+  (match form
+    [(<form> unit-name title fields)
+        (define template_ (prepare-template file-code-template unit-name))
+        (format template_ (apply ~a #:separator "\n" `(,@(map field->code fields))) "//write functions here nigger")]
+    [_ (raise "wrong pattern boy")]))
 
-(struct <form> ( unit-name
-                 title
-                 fields ))
-                 
+;(pretty-display (field->code (car (<form>-fields informacion-para-efectuar-liquidacion))))
+; (pretty-display (mk-file-code informacion-para-efectuar-liquidacion))
