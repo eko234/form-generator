baf76df0bc0d2aa58e5874585af35bbe840b0f3b
jujo <lu.al.bu@hotmail.com>
2020-11-26 10:52:39 -0500

    skeleteon almost done

--- main.rkt
+++ main.rkt
@@ -7,10 +7,26 @@
 
 (define file-design-template
 "
+inheritedTFRM<UNITNAME>
+  clientHeight = 600
+  clientWidth = 1000
+  caption = ~a
+  BorderStyle = bsNone
+  FormStyle = fsMDIChild
+  Position = poScreenCenter
+  Visible = True
+  Layout = 'form'
+  ExplicitWidth = 1000
+  ExplicitHeight = 600
+  PixelPerInch = 96
+  TextHeight = 13
+~a
+end
 ")
 
 (define file-code-template ; <UNITNAME> must be replaced before using the returned template
-"unit <UNITNAME>;
+"
+unit <UNITNAME>;
 interface
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics,
@@ -44,18 +60,24 @@ end.
     (define template_ (prepare-template file-design-template (<form>-title form)))
     (raise "TODO"))
 
-(struct <field> (label input-type source options constraints))
+(struct <field> (label input-type source order options constraints))
 
 (struct <form> ( unit-name
                  title
                  fields ))
 
-(define (field->design field)
-  (raise "TODO"))
+
+(define (field-position o)
+  "TODO")
+(define (label-position o)
+  "TODO")
+
+(define (field-properties field)
+  "TODO")
 
 (define (field->code field)
   (match field
-     [(<field> label input-type source options constraints)
+     [(<field> label input-type source order options constraints)
       (~a #:separator "\n" 
           (format "    uniLabel~a: TUniLabel;" source)
           (match input-type
@@ -66,8 +88,26 @@ end.
            ['PICK-F (format "    uniDBComboBox~a: TUniDBComboBox;"             source)]
            ['PICK-S (format "    uniDBComboBox~a: TUniDBComboBox;"             source)]))]))
 
-(define (mk-<field> label input-type source [options null] [constraints null])
-    (<field> label input-type source options constraints))
+(define (field->design field)
+  (match field
+     [(<field> label input-type source order options constraints)
+      (~a #:separator "\n" 
+          (format "    inherited uniLabel~a: TUniLabel" source)
+          (label-position order)  
+          "end"
+          (match input-type
+           ['STR    (format "    inherited uniDBEdit~a: TUniDBEdit"                     source)]
+           ['INT    (format "    inherited uniDBNumberEdit~a: TUniDBNumberEdit"         source)]
+           ['CASH   (format "    inherited uniDBNumberEdit~a: TUniDBNumberEdit"         source)]
+           ['DATE   (format "    inherited uniDBDateTimePicker~a: TUniDBDateTimePicker" source)]
+           ['PICK-F (format "    inherited uniDBComboBox~a: TUniDBComboBox"             source)]
+           ['PICK-S (format "    inherited uniDBComboBox~a: TUniDBComboBox"             source)])
+          (field-position order)
+          (field-properties field)
+          "end")]))
+
+(define (mk-<field> label input-type source order [options null] [constraints null])
+    (<field> label input-type source order options constraints))
 
 ; add contract to make options mandatory if pick fixed is chosed
 ; options can be a select statement that determines the posible values a select can take
@@ -75,11 +115,11 @@ end.
 (define informacion-para-efectuar-liquidacion
     (<form> "INFORMACION_LIQUIDACION"
             "informacion para efectuar liquidacion" 
-            (list (mk-<field> "Vigencia"            'DATE        'LIQ-VIG  )
-                  (mk-<field> "Actividad Economica" 'PICK-S      'ACT-ECO "select * from actividades-economicas" )
-                  (mk-<field> "Ingresos Brutos"     'CASH        'LIQ-ING )
-                  (mk-<field> "Ingresos Fuera Mun"  'CASH        'LIQ-ING-EXT )
-                  (mk-<field> "Ingresos Netos"      'CASH        'LIQ-ING ))))
+            (list (mk-<field> "Vigencia"            'DATE   'LIQ-VIG     1 )
+                  (mk-<field> "Actividad Economica" 'PICK-S 'ACT-ECO     2  "select * from actividades-economicas" )
+                  (mk-<field> "Ingresos Brutos"     'CASH   'LIQ-ING     3 )
+                  (mk-<field> "Ingresos Fuera Mun"  'CASH   'LIQ-ING-EXT 4 )
+                  (mk-<field> "Ingresos Netos"      'CASH   'LIQ-ING     5 ))))
 
 ; the most important thing is the datasource of the liquidation, there the checks must be very precise
                 
